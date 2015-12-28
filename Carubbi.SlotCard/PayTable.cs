5fa16bdc939ea12ab4be6d7078c83b17da41490c
rcarubbi <rcarubbi@gmail.com>
2015-12-28 12:00:14 -0200

    Update PayTable.cs

--- Carubbi.SlotCard/PayTable.cs
+++ Carubbi.SlotCard/PayTable.cs
@@ -5,13 +5,13 @@ using System.Text;
 
 namespace Carubbi.SlotCard
 {
-    public class PayTable 
+   public class PayTable 
     {
-        public static Int32 Pair = 0;
-        public static Int32 TwoPairs = 0;
-        public static Int32 ThreeOfAKind = 0;
-        public static Int32 Straight = 0;
-        public static Int32 Flush = 0;
+        public static Int32 Pair = 2;
+        public static Int32 TwoPairs = 5;
+        public static Int32 ThreeOfAKind = 10;
+        public static Int32 Straight = 25;
+        public static Int32 Flush = 50;
         public static Int32 Fullen = 100;
         public static Int32 FourOfAKind = 300;
         public static Int32 StraightFlush = 10000;
